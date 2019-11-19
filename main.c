e1d3f0cd91c43b6b24dabf0b0faa97de39181677
Woojung618 <eyes_cat@naver.com>
2019-11-19 15:30:11 +0900

    4

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void main(int argc, char *argv[]) {
-	char src[]="The worst things to eat before you sleep";
-	char dst[100];
-	strcpy(dst,src);
-	printf("copied string : %s",dst);
+	char str[]="happy C programming";
+	
+	printf("문자열\"%s\"의 길이: %i",str,strlen(str));
 }
